b02b148013a6e0d1d6e9ced66bf5e475e535443e
OtakuNekoP <otakunekop@52miku.com>
2016-01-04 16:02:29 +0800

    new
    new

---
+++ start.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import CGIHTTPServer
+import BaseHTTPServer
+from optparse import OptionParser
+
+class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
+    cgi_directories = ["/cgi-bin"]
+
+PORT = 8081
+
+def main():
+    parser = OptionParser()
+    opts, args = parser.parse_args()
+    if args:
+        httpd = BaseHTTPServer.HTTPServer((args[0], int(args[1])), Handler)
+        print "Python-web server running at http://%s:%s/" % (args[0], args[1])
+    else:
+        httpd = BaseHTTPServer.HTTPServer(("0.0.0.0", PORT), Handler)
+        print "Python-web server running at http://%s:%s/" % ("0.0.0.0", PORT)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
